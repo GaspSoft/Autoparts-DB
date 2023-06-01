c1bdaaf41fe56fa60692efc69e8aab9650704383
Nicolas Maba <nicolas.rm2005@aluno.ifsc.edu.br>
2023-06-01 10:15:46 -0300

    inserts fornecedor

---
+++ fornecedores.sql
@@ -0,0 +1,28 @@
+create table fornecedores (
+	nome VARCHAR(50)
+);
+insert into fornecedores (nome) values ('Loise Menicomb');
+insert into fornecedores (nome) values ('Newton Ducket');
+insert into fornecedores (nome) values ('Emilio Verty');
+insert into fornecedores (nome) values ('Catarina Vane');
+insert into fornecedores (nome) values ('Benedikta Sonier');
+insert into fornecedores (nome) values ('Kylila Kirton');
+insert into fornecedores (nome) values ('Jesus Cunniam');
+insert into fornecedores (nome) values ('Aprilette Denver');
+insert into fornecedores (nome) values ('Ana Piechnik');
+insert into fornecedores (nome) values ('Conni Mustchin');
+insert into fornecedores (nome) values ('Quinlan Ashborn');
+insert into fornecedores (nome) values ('Morry Effaunt');
+insert into fornecedores (nome) values ('Cristy Benedit');
+insert into fornecedores (nome) values ('Charmane Bacop');
+insert into fornecedores (nome) values ('Ranee Bernette');
+insert into fornecedores (nome) values ('Candie Maffey');
+insert into fornecedores (nome) values ('Stanislas Rouse');
+insert into fornecedores (nome) values ('Tabb Boddington');
+insert into fornecedores (nome) values ('Tanner Gerardot');
+insert into fornecedores (nome) values ('Cordie Luten');
+insert into fornecedores (nome) values ('Debera Peers');
+insert into fornecedores (nome) values ('Boycie Le Strange');
+insert into fornecedores (nome) values ('Cathlene Dreossi');
+insert into fornecedores (nome) values ('Lolly Kirk');
+insert into fornecedores (nome) values ('Sawyer Bowmer');
