89c0a27eb383d2f52ba1b395ebb844b64347ad5f
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-06-12 08:12:45 -0300

    Deletando patas

--- DELETE/pecas_deletes.sql
+++
@@ -1,7 +0,0 @@
-USE autoparts_db;
-
-DELETE FROM pecas WHERE Pecas_id = 1;
-DELETE FROM pecas WHERE Pecas_id = 2;
-DELETE FROM pecas WHERE Pecas_id = 3;
-DELETE FROM pecas WHERE Pecas_id = 4;
-DELETE FROM pecas WHERE Pecas_id = 5;
