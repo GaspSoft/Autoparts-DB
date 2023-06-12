c85d1d9b48e4b77062b2f1f5750a8d96a17843f5
Nicolas <nicolas.rm2005@aluno.ifsc.edu.br>
2023-06-12 00:22:33 -0300

    select all e select count(*) das tabelas

--- COMANDOS/Fornecedores.sql
+++
@@ -1,9 +0,0 @@
-select count(*) from fornecedores;
-
-select * from fornecedores order by fornecedor_id asc;
-
-select fornecedores.fornecedor_id, vendas.pecas_fornecedores_id 
-from fornecedores 
-inner join  vendas
-on 
-fornecedores.fornecedor_id = vendas.pecas_fornecedores_id;
