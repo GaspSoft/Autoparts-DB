cffdaad10ebe5234b697fc5a8848dc929e878db3
Rafael Ronchi <rafael.e01@aluno.ifsc.edu.br>
2023-06-12 08:12:45 -0300

    Deletando pastas

--- DELETES/endereco_insert.sql
+++
@@ -1,5 +0,0 @@
-delete from Funcionarios where funcionario_id = 1;
-delete from Funcionarios where funcionario_id = 2;
-delete from Funcionarios where funcionario_id = 3;
-delete from Funcionarios where funcionario_id = 4;
-delete from Funcionarios where funcionario_id = 5;
